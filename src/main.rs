80200536fa07217f2043545af9a0603c580c4ebd
mc962 <11969409+mc962@users.noreply.github.com>
2020-06-11 21:26:29 -0400

    Start building orbtk dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,52 @@
-use std::process::exit;
-// StructOpt import is required here for using its from_args method with Options
-use structopt::StructOpt;
-
-pub mod hashing;
-use hashing::{hash_file, hash_matches};
-
-mod cli;
-use cli::{Options, algorithm_type};
+use orbtk::prelude::*;
 
 fn main() {
-    let args = Options::from_args();
-    let algorithm = match algorithm_type(&args.method) {
-        Ok(algorithm) => algorithm,
-        Err(reason) => {
-            println!("Problem determining hashing algorithm: {}", reason);
-            exit(1);
-        }
-    };
-
-    let hash_str = hash_file(algorithm, &args.path);
-    match hash_str {
-        Ok(hash) => {
-            println!("{}   {}", hash, args.path.display());
-            if hash_matches(&hash, &args.expected) {
-                println!("SUCCESS: file hash matches expected checksum hash!")
-            } else {
-                println!("WARNING: File hash does not match expected checksum hash!");
-                println!("Expected: {} | Actual: {}", args.expected, hash);
-            }
-        },
-        Err(reason) => {
-            println!("Problem hashing file: {}", reason);
-            exit(1);
-        }
-    }
+    Application::new()
+        .window(|ctx| {
+            Window::create()
+                .title("Checkasum")
+                .position((100.0, 100.0))
+                .size(420.0, 730.0)
+                .child(TextBlock::create().text("Checkasum").build(ctx))
+                .build(ctx)
+        })
+        .run();
 }
+
+// use std::process::exit;
+// // StructOpt import is required here for using its from_args method with Options
+// use structopt::StructOpt;
+//
+// pub mod hashing;
+// use hashing::{hash_file, hash_matches};
+//
+// mod cli;
+// use cli::{Options, algorithm_type};
+//
+// fn main() {
+//     let args = Options::from_args();
+//     let algorithm = match algorithm_type(&args.method) {
+//         Ok(algorithm) => algorithm,
+//         Err(reason) => {
+//             println!("Problem determining hashing algorithm: {}", reason);
+//             exit(1);
+//         }
+//     };
+//
+//     let hash_str = hash_file(algorithm, &args.path);
+//     match hash_str {
+//         Ok(hash) => {
+//             println!("{}   {}", hash, args.path.display());
+//             if hash_matches(&hash, &args.expected) {
+//                 println!("SUCCESS: file hash matches expected checksum hash!")
+//             } else {
+//                 println!("WARNING: File hash does not match expected checksum hash!");
+//                 println!("Expected: {} | Actual: {}", args.expected, hash);
+//             }
+//         },
+//         Err(reason) => {
+//             println!("Problem hashing file: {}", reason);
+//             exit(1);
+//         }
+//     }
+// }
