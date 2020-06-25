3a6727b69b2a2e1416d9da7cc593aa0d9e741787
mc962 <11969409+mc962@users.noreply.github.com>
2020-06-24 22:40:00 -0400

    Save electron-builder config.

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -5,6 +5,7 @@ pub mod hashing;
 
 use hashing::check_file;
 
+#[no_mangle]
 pub fn gui_check_file(mut cx: FunctionContext) -> JsResult<JsBoolean> {
     let method = cx.argument::<JsString>(0).unwrap().value();
     let path = cx.argument::<JsString>(1).unwrap().value();
