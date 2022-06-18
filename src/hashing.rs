0a563aa382630ee91a17bc1d17b0b3e6e2faa73a
mc962 <11969409+mc962@users.noreply.github.com>
2022-06-18 14:34:27 -0400

    Switch to rustcrypto

--- src/hashing.rs
+++ src/hashing.rs
@@ -1,14 +1,18 @@
+use std::{io};
 use data_encoding::HEXLOWER;
-use ring::digest::{Context, SHA256};
+// use ring::digest::{Context, SHA256};
 use std::fs::File;
 use std::io::{BufReader, Error, Read};
 use std::path::Path;
+use md5::Md5;
+use sha2::{Sha256, Digest};
 
 use crate::error::OptionError;
 
 /// Allowed hashing algorithms
 pub enum HashAlgorithm {
     SHA256,
+    MD5
 }
 
 /// Checks if hash generated from file matches the documented expected hash
@@ -42,7 +46,8 @@ pub fn hash_file(hashing_method: HashAlgorithm, path: &Path) -> Result<String, E
     let reader = file_reader(path)?;
 
     match hashing_method {
-        HashAlgorithm::SHA256 => Ok(hash_sha256(reader)),
+        HashAlgorithm::SHA256 => hash_sha256(reader, path),
+        HashAlgorithm::MD5 => hash_md5(reader, path)
     }
 }
 
@@ -59,28 +64,80 @@ pub fn hash_file(hashing_method: HashAlgorithm, path: &Path) -> Result<String, E
 /// # Errors
 ///
 /// Reader may encounter potential I/O or other errors
-fn hash_sha256<R: Read>(mut reader: R) -> String {
-    // set up new context object for SHA256 Init-Update-Finish operations
-    let mut context = Context::new(&SHA256);
-    // maintain a buffer for reading for performance
-    let mut buffer = [0; 1024];
-    loop {
-        match reader.read(&mut buffer) {
-            Ok(count) => {
-                if count == 0 {
-                    break;
-                }
-                context.update(&buffer[..count])
-            }
-            Err(reason) => {
-                println!("Problem hashing file contents: {}", reason);
-                break;
-            }
-        }
-    }
+fn hash_sha256<R: Read>(mut _reader: R, path: &Path) -> Result<String, Error> {
+    let mut file = File::open(&path)?;
+    let mut hasher = Sha256::new();
 
-    let digest = context.finish();
-    HEXLOWER.encode(digest.as_ref())
+    let _data = io::copy(&mut file, &mut hasher)?;
+    let digest = hasher.finalize();
+
+    Ok(HEXLOWER.encode(digest.as_ref()))
+    // // set up new context object for SHA256 Init-Update-Finish operations
+    // let mut context = Context::new(&SHA256);
+    // // maintain a buffer for reading for performance
+    // let mut buffer = [0; 1024];
+    // loop {
+    //     match reader.read(&mut buffer) {
+    //         Ok(count) => {
+    //             if count == 0 {
+    //                 break;
+    //             }
+    //             context.update(&buffer[..count])
+    //         }
+    //         Err(reason) => {
+    //             println!("Problem hashing file contents: {}", reason);
+    //             break;
+    //         }
+    //     }
+    // }
+    //
+    // let digest = context.finish();
+    // HEXLOWER.encode(digest.as_ref())
+}
+
+/// Hashes a file with the MD5 algorithm
+///
+/// # Examples
+///
+/// ```
+/// use hashing::{hash_md5, file_reader};
+/// let reader = file_reader("/bob/path/to/file.iso");
+/// hash_md5(reader);
+/// ```
+///
+/// # Errors
+///
+/// Reader may encounter potential I/O or other errors
+fn hash_md5<R: Read>(mut _reader: R, path: &Path) -> Result<String, Error> {
+    let mut file = File::open(&path)?;
+    let mut hasher = Md5::new();
+
+    let _data = io::copy(&mut file, &mut hasher)?;
+    let digest = hasher.finalize();
+
+    Ok(HEXLOWER.encode(digest.as_ref()))
+
+    // // set up new context object for SHA256 Init-Update-Finish operations
+    // let mut context = Context::new(&SHA256);
+    // // maintain a buffer for reading for performance
+    // let mut buffer = [0; 1024];
+    // loop {
+    //     match reader.read(&mut buffer) {
+    //         Ok(count) => {
+    //             if count == 0 {
+    //                 break;
+    //             }
+    //             context.update(&buffer[..count])
+    //         }
+    //         Err(reason) => {
+    //             println!("Problem hashing file contents: {}", reason);
+    //             break;
+    //         }
+    //     }
+    // }
+    //
+    // let digest = context.finish();
+    // HEXLOWER.encode(digest.as_ref())
 }
 
 /// Reads a file from a supplied file path, and adds it to a Buffered Reader for efficient file reading
@@ -126,6 +183,7 @@ fn file_reader(path: &Path) -> Result<BufReader<File>, Error> {
 pub fn algorithm_type(method: &str) -> Result<HashAlgorithm, OptionError> {
     match method {
         "sha256" => Ok(HashAlgorithm::SHA256),
+        "md5" => Ok(HashAlgorithm::MD5),
         err => Err(OptionError::new(format!("unknown action: {}", err))),
     }
 }
