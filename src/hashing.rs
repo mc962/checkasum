5d84ece1fae26f808e98274e7c193f8251cf6439
mc962 <11969409+mc962@users.noreply.github.com>
2022-06-18 18:00:20 -0400

    Eliminate bufreader

--- src/hashing.rs
+++ src/hashing.rs
@@ -1,8 +1,7 @@
 use std::{io};
 use data_encoding::HEXLOWER;
-// use ring::digest::{Context, SHA256};
 use std::fs::File;
-use std::io::{BufReader, Error, Read};
+use std::io::{Error};
 use std::path::Path;
 use md5::Md5;
 use sha2::{Sha256, Digest};
@@ -43,11 +42,9 @@ pub fn hash_matches(file_hash: &str, correct_hash: &str) -> bool {
 /// Bubbles up errors from file_reader
 /// Bubbles up errors from attempting file hashing
 pub fn hash_file(hashing_method: HashAlgorithm, path: &Path) -> Result<String, Error> {
-    let reader = file_reader(path)?;
-
     match hashing_method {
-        HashAlgorithm::SHA256 => hash_sha256(reader, path),
-        HashAlgorithm::MD5 => hash_md5(reader, path)
+        HashAlgorithm::SHA256 => hash_sha256(path),
+        HashAlgorithm::MD5 => hash_md5(path)
     }
 }
 
@@ -64,35 +61,15 @@ pub fn hash_file(hashing_method: HashAlgorithm, path: &Path) -> Result<String, E
 /// # Errors
 ///
 /// Reader may encounter potential I/O or other errors
-fn hash_sha256<R: Read>(mut _reader: R, path: &Path) -> Result<String, Error> {
+fn hash_sha256(path: &Path) -> Result<String, Error> {
     let mut file = File::open(&path)?;
     let mut hasher = Sha256::new();
 
     let _data = io::copy(&mut file, &mut hasher)?;
+
     let digest = hasher.finalize();
 
     Ok(HEXLOWER.encode(digest.as_ref()))
-    // // set up new context object for SHA256 Init-Update-Finish operations
-    // let mut context = Context::new(&SHA256);
-    // // maintain a buffer for reading for performance
-    // let mut buffer = [0; 1024];
-    // loop {
-    //     match reader.read(&mut buffer) {
-    //         Ok(count) => {
-    //             if count == 0 {
-    //                 break;
-    //             }
-    //             context.update(&buffer[..count])
-    //         }
-    //         Err(reason) => {
-    //             println!("Problem hashing file contents: {}", reason);
-    //             break;
-    //         }
-    //     }
-    // }
-    //
-    // let digest = context.finish();
-    // HEXLOWER.encode(digest.as_ref())
 }
 
 /// Hashes a file with the MD5 algorithm
@@ -108,7 +85,7 @@ fn hash_sha256<R: Read>(mut _reader: R, path: &Path) -> Result<String, Error> {
 /// # Errors
 ///
 /// Reader may encounter potential I/O or other errors
-fn hash_md5<R: Read>(mut _reader: R, path: &Path) -> Result<String, Error> {
+fn hash_md5(path: &Path) -> Result<String, Error> {
     let mut file = File::open(&path)?;
     let mut hasher = Md5::new();
 
@@ -116,50 +93,6 @@ fn hash_md5<R: Read>(mut _reader: R, path: &Path) -> Result<String, Error> {
     let digest = hasher.finalize();
 
     Ok(HEXLOWER.encode(digest.as_ref()))
-
-    // // set up new context object for SHA256 Init-Update-Finish operations
-    // let mut context = Context::new(&SHA256);
-    // // maintain a buffer for reading for performance
-    // let mut buffer = [0; 1024];
-    // loop {
-    //     match reader.read(&mut buffer) {
-    //         Ok(count) => {
-    //             if count == 0 {
-    //                 break;
-    //             }
-    //             context.update(&buffer[..count])
-    //         }
-    //         Err(reason) => {
-    //             println!("Problem hashing file contents: {}", reason);
-    //             break;
-    //         }
-    //     }
-    // }
-    //
-    // let digest = context.finish();
-    // HEXLOWER.encode(digest.as_ref())
-}
-
-/// Reads a file from a supplied file path, and adds it to a Buffered Reader for efficient file reading
-///
-/// # Examples
-///
-/// ```
-/// use hashing::file_reader;
-/// let reader = file_reader("/bob/path/to/file.iso");
-/// ```
-///
-/// # Errors
-///
-/// May encounter error when attempting to open file (missing file, other I/O error)
-/// ```
-/// use hashing::file_reader;
-/// let reader = file_reader("/bob/unknown/file/path");
-/// ```
-fn file_reader(path: &Path) -> Result<BufReader<File>, Error> {
-    let in_file = File::open(path)?;
-
-    Ok(BufReader::new(in_file))
 }
 
 /// Select algorithm type to use for hashing file, based on CLI input
